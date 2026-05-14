e45d8b2e82252b2ed4ba23767c8e7bd4a58c0ee7
galih yasa <wannlearning707@gmail.com>
2026-05-14 23:19:08 +0800

    Create welcome.blade.php

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Mini Chat</title>
+</head>
+<body>
+    <h1>Mini Real-Time Chat Laravel Reverb</h1>
+    <p>Aplikasi mini chat real-time menggunakan Laravel dan Reverb.</p>
+    <a href="/">Masuk ke Chat</a>
+</body>
+</html>
