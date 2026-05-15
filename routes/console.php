77d85b7363fb6a7f6d2370985e3e90f12ec436c5
galih yasa <wannlearning707@gmail.com>
2026-05-15 22:17:42 +0800

    Delete routes directory

--- routes/console.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Foundation\Console\ClosureCommand;
-use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('inspire', function () {
-    $this->comment('Mini Real-Time Chat Laravel Reverb');
-})->purpose('Display an inspiring message');
